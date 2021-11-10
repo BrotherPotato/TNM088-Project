73ef8f3aa9123ed824aa2409b5c1ccd13011ef3f
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2021-11-10 21:13:38 +0100

    Made A Test Game

--- Project/Assets/Bird.cs
+++ Project/Assets/Bird.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Bird : MonoBehaviour
 {
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,33 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+    */
+    Vector3 _initialPosition;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * 100);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+
     }
 }
