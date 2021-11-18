2f79edde2e481d002c3f3885771de3f12a38cbb4
PhilipRobertsson <phiro138@student.liu.se>
2021-11-18 11:56:20 +0100

    Physics programming
    Proggramerade kollisioner och lite player movements 10.15-12

---
+++ Project/Assets/Testing/collisions.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisions : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    // Start is called before the first frame update
+    void Start() {
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.tag=="ground"){
+            Debug.Log("hej");
+            playerRb.gravityScale = 0;
+            playerRb.velocity = new Vector3(0f, 0, 0);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col){
+        playerRb.gravityScale = 1;
+    }
+}
---
+++ Project/Assets/Testing/playerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 1f;
+    public float jumpForce = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate(){
+        playerRb.velocity= new Vector2(horizontal*runSpeed, vertical * jumpForce);
+    }
+}
