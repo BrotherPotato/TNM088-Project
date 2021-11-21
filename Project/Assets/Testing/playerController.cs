9a14e9f886408f5bfe8b3c48492cb81cceab9ebd
PhilipRobertsson <phiro138@student.liu.se>
2021-11-21 10:55:05 +0100

    Blandade tillägg
    Player movement har nu utökats och allt grafiskt matrial som ligger på driven just nu är uppladdat här

--- Project/Assets/Testing/playerController.cs
+++ Project/Assets/Testing/playerController.cs
@@ -4,26 +4,77 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public Rigidbody2D playerRb;
-    
-    float horizontal;
-    float vertical;
+    Rigidbody2D rb; 
+    public float speed;
+    public float jumpheigth;
+    bool isGrounded = false; 
+    public Transform isGroundedChecker; 
+    public float checkGroundRadius; 
+    public LayerMask groundLayer;
+    public float fallMultiplier = 9.82f; 
+    public float lowJumpMultiplier = 2f;
+    public float rememberGroundedFor; 
+    float lastTimeGrounded;
+    public int defaultAdditionalJumps = 1; 
+    int additionalJumps;
 
-    public float runSpeed = 1f;
-    public float jumpForce = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        Move(); 
+       Jump();
+       BetterJump();
+       CheckIfGrounded();
     }
-    private void FixedUpdate(){
-        playerRb.velocity= new Vector2(horizontal*runSpeed, vertical * jumpForce);
+
+    void Move() 
+    { 
+        float x = Input.GetAxisRaw("Horizontal"); 
+        float moveBy = x * speed; 
+        rb.velocity = new Vector2(moveBy, rb.velocity.y); 
+
+    }
+
+    void Jump() 
+    { 
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0)) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpheigth);
+            additionalJumps--;
+            if(Input.GetKeyDown(KeyCode.Space) && !isGrounded && additionalJumps == 0){
+                rb.gravityScale = 0.5f;
+            }
+         }
+     } 
+
+    void CheckIfGrounded()
+    { 
+        Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer); 
+        if (colliders != null) { 
+            isGrounded = true; 
+            additionalJumps = defaultAdditionalJumps;
+            rb.gravityScale = 1f;
+        } 
+        else 
+        { 
+            if (isGrounded) { 
+                lastTimeGrounded = Time.time; 
+            } 
+            isGrounded = false; 
+        } 
+    }
+    void BetterJump() {
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        } 
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }   
     }
 }
