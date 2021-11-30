d8ecb12e3275442aaa6e0ac003280baa15c7d80e
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2021-11-30 21:15:11 +0100

    did some selecto tings

--- Project/Assets/Projektet/LevelSelectionThings/LevelSelector.cs
+++ Project/Assets/Projektet/LevelSelectionThings/LevelSelector.cs
@@ -8,10 +8,15 @@ public class LevelSelector : MonoBehaviour
 {
     public SceneFader fader;
 
-    public void Select (string levelName)
+    public void Select(string levelName)
     {
         Debug.Log(levelName);
         fader.FadeTo(levelName);
         //SceneManager.LoadScene(levelName);
+    }
+    public void Quit()
+    {
+        Debug.Log("Helo");
+        Application.Quit();
     }
 }
